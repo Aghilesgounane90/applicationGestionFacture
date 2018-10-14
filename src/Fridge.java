46ad5e8a88532399b29706fd18b7641b9c3448ed
Aghilesgounane90 <aghiles.gounane@outlook.fr>
2018-10-14 10:33:45 +0200

    ajout les classes Fridge et Television qui hérite de la classe Product

---
+++ src/Fridge.java
@@ -0,0 +1,28 @@
+public class Fridge extends Product {
+
+    public Fridge(String name, String description, double price, int liter, boolean freezer) {
+        super(name, description, price);
+        this.liter = liter;
+        this.freezer = freezer;
+    }
+
+    int liter;
+    boolean freezer;
+
+    public int getLiter() {
+        return liter;
+    }
+
+    public void setLiter(int liter) {
+        this.liter = liter;
+    }
+
+    public boolean isFreezer() {
+        return freezer;
+    }
+
+    public void setFreezer(boolean freezer) {
+        this.freezer = freezer;
+    }
+
+}
---
+++ src/Television.java
@@ -0,0 +1,27 @@
+public class Television extends Product {
+
+    public Television(String name, String description, double price, int size, String slabType) {
+        super(name, description, price);
+        this.size = size;
+        this.slabType = slabType;
+    }
+
+    int size;
+    String slabType;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getSlabType() {
+        return slabType;
+    }
+
+    public void setSlabType(String slabType) {
+        this.slabType = slabType;
+    }
+}
